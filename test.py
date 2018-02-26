bbdc84f57d723d48d4c6a3b4b86db81c049d9c42
Oriol Romani <oriol.romani@upf.edu>
2018-02-26 15:01:23 +0100

    Change views, add message

--- test.py
+++ test.py
@@ -8,26 +8,50 @@ def home():
     return "Test app for chrome issue"
 
 
-@app.route("/same", methods=['GET', 'POST'])
+@app.route("/test", methods=['GET', 'POST'])
 def same():
-    return render_template('same.html')
+    return render_template('microphone_test.html')
 
 
-@app.route("/iframe_source")
-def iframe_source():
+# Using iframe's source
+
+@app.route("/iframe_src")
+def iframe_src_same():
     return render_template('iframe_source.html')
 
 
-@app.route("/iframe_form")
-def iframe_form():
-    return render_template('iframe_form.html')
+@app.route("/iframe_src_diff")
+def iframe_src_diff():
+    return render_template('iframe_source_diff.html')
 
 
-@app.route("/cross")
-def cross():
-    return render_template('cross.html')
+@app.route("/iframe_src_diff_allow")
+def iframe_src_diff_allow():
+    return render_template('iframe_source_diff_allow.html')
 
 
-@app.route("/cross_source")
-def cross_source():
-    return render_template('cross_source.html')
+@app.route("/iframe_src_diff_header")
+def iframe_src_diff_header():
+    return render_template('iframe_source_diff_header.html')
+
+# Using a form to fill the iframe
+
+
+@app.route("/get_from_same")
+def get_from_same():
+    return render_template('get_from_same.html')
+
+
+@app.route("/post_from_same")
+def post_from_same():
+    return render_template('post_from_same.html')
+
+
+@app.route("/get_from_diff")
+def get_from_diff():
+    return render_template('get_from_diff.html')
+
+
+@app.route("/post_from_diff")
+def post_from_diff():
+    return render_template('post_from_diff.html')
